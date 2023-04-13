3396dc188a7ccad56e8ef18e7ee37364491e06d6
Kenjie <kbalona5@gmail.com>
2023-04-13 11:37:52 +0800

    add sms

--- app/src/main/java/com/example/menu/MainActivity.java
+++ app/src/main/java/com/example/menu/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,13 +14,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btn1 = findViewById(R.id.btnMenu);
+        Button sms = findViewById(R.id.sms);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,MainActivity2_Menu.class);
-                startActivity(intent);
-            }
+        btn1.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this,MainActivity2_Menu.class);
+            startActivity(intent);
+        });
+
+        sms.setOnClickListener(v -> {
+            Intent intent1 = new Intent(MainActivity.this, Activity_SendSMSPage.class);
+            startActivity(intent1);
         });
     }
 
